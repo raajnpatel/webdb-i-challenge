92e95520c0dcc80a39b563782f05f175c1a45ed5
rndpatel@gmail.com <rndpatel@gmail.com>
2019-12-09 14:47:49 -0800

    GET operations complete - returning 200

--- server.js
+++ server.js
@@ -6,4 +6,39 @@ const server = express();
 
 server.use(express.json());
 
+// Get All Accounts
+server.get('/', (req,res)=>{
+    db('accounts')
+        .then(accounts => {
+            res
+                .status(200)
+                .json(accounts)
+        })
+        .catch( error => {
+            console.log(error);
+            res
+                .status(500)
+                .json({ message: "Error retrieving accounts from database" });
+        })
+});
+
+// Get One Account
+server.get('/:id', (req,res)=>{
+    db('accounts')
+        .where({id: req.params.id})
+            .first()
+            .then(account => {
+                res
+                    .status(200)
+                    .json(account)
+            })
+            .catch( error => {
+                console.log(error);
+                res
+                    .status(500)
+                    .json({ message: "Error retrieving accounts from database" });
+            })
+});
+
+
 module.exports = server;
